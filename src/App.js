7f6576c3ccf81c68ecc3b6a055ac39629e0bcbfb
Tayipztrk <tayipztrk@icloud.com>
2021-06-03 12:38:43 +0300

    first version

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import SearchBar from './components/SearchBar'
+import youtube from './apis/youtube'
+import VideoList from './components/VideoList'
+import VideoDetail from './components/VideoDetail'
 
-function App() {
+class App extends React.Component {
+
+    state = { videos: [], selectedVideo: null}
+
+    componentDidMount(){
+      this.onTermSubmit("buildings")
+    }
+
+    onTermSubmit = async (term) => {
+      const response = await youtube.get("/search", {
+        params: {
+          q: term,
+        },
+      });
+
+      this.setState({
+        videos: response.data.items,
+        selectedVideo: response.data.items[0],
+      });
+    };
+    
+    
+    onVideoSelect = (video) => {
+      this.setState({selectedVideo: video });
+    };
+
+   
+
+  render(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="ui container">
+       <SearchBar onFormSubmit={this.onTermSubmit}/> 
+       <div className="ui grid">
+         <div className="ui row">
+           <div className="eleven wide column">
+             <VideoDetail video={this.state.selectedVideo}/>
+           </div>
+           <div className="five wide column">
+           <VideoList
+              onVideoSelect={this.onVideoSelect}
+              videos={this.state.videos}
+           />
+           </div>
+         </div>
+       </div>
     </div>
   );
+  }
 }
 
 export default App;
